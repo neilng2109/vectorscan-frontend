a23f143feb2ee27e9dd8b5a11aef10fe34dbae29
Neil Gordon <neilng2109@gmail.com>
2025-08-03 11:10:44 +0200

    Fix button icon size with inline styles to prevent oversized display

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -196,7 +196,7 @@ const LandingPage = () => {
                       </>
                     ) : (
                       <>
-                        <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20">
+                        <svg className="w-4 h-4 mr-2 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20" style={{width: '16px', height: '16px'}}>
                           <path fillRule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
                         </svg>
                         Access Dashboard
