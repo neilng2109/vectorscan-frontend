58ac03c119569c51b9552d6f051e137ea75b4c5e
Neil Gordon <neilng2109@gmail.com>
2025-09-08 15:56:43 +0200

    Fix Procfile to use query_pinecone:app and remove working_api reference

--- query_pinecone.py
+++ query_pinecone.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 import os
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from safe_ai_query import query_fault_description_safe
